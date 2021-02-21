4c945d56cfec958cbc17169c38f1188994cc955c
Jasur Amirov <ajasur@gmail.com>
2021-02-20 23:18:20 -0800

    added license info and contact section

--- README.md
+++ README.md
@@ -1,7 +1,27 @@
-# Readme Generator
+# Readme Generator ![GitHub license](https://img.shields.io/github/license/Naereen/StrapDown.js.svg)
 ## Description 
-It creates a dynamic readme file using command line
+* It creates a dynamic readme file using teminal commainds.
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+
 ## Installation
-Just copy this repo into your local mashine . No Installation is neeeded.
+* npm install.
 ## Usage
-Useful for creating readme.md file just entering the alll custon info for your readme.
+* It can be used to create readme.md file promptly.
+## Credits
+
+* I used npm inquirer for this project.
+
+## License
+
+* MIT.
+
+
+## Questions
+
+If you have questions contact me on [GitHub](jamirov.github.io) or contact Jasur at ajasur@gmail.com.
+
--- index.js
+++ index.js
@@ -22,33 +22,82 @@ const questions = [{
     name: 'usage',
     message: 'Please write Usage information',
   },
-
-
-
+  {
+    type: 'input',
+    name: 'credits',
+    message: 'Please enter your credits.',
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "What kind of license should your project have?",
+    choices: ["MIT","APACHE 2.0","GPL 3.0","BSD 3","None"]
+},
+  
+  {
+    type: 'input',
+    name: 'githublink',
+    message: 'Please enter your github profile link',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Please enter your email',
+  },
+  {
+    type: 'input',
+    name: 'userName',
+    message: 'Please enter your name',
+  },
 ];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions)
     .then((response) => {
+      let badge = "";
+      if(response.license == "MIT"){
+          badge = "![GitHub license](https://img.shields.io/github/license/Naereen/StrapDown.js.svg)"
+      }else if (response.license == "APACHE 2.0"){
+          badge = "![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)"
+      }else if (response.license == "GPL 3.0"){
+          badge = "![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)"
+      }else if (response.license == "BSD 3"){
+          badge = "![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)"
+      }
 const string = 
-    `# ${response.projectName}
+    `# ${response.projectName} ${badge}
 ## Description 
-${response.description}
+* ${response.description}.
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+
 ## Installation
-${response.installInstruction}
+* ${response.installInstruction}.
 ## Usage
-${response.usage}`
-    
+* ${response.usage}.
+## Credits
+
+* ${response.credits}.
+
+## License
+
+* ${response.license}.
+
+
+## Questions
+
+If you have questions contact me on [GitHub](${response.githublink}) or contact ${response.userName} at ${response.email}.
+
+`
     fs.writeFile('README.md', string, (err) =>
     err ? console.error(err) : console.log('README Generated!')
     );
-    
+  
 });
 }
-
 // Function call to initialize app
 init();
